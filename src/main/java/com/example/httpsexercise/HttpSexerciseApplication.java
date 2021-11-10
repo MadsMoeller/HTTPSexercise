733fdd349ab4ef265366525bb68dcc335519fad5
Mads Møller <67467953+MadsMoeller@users.noreply.github.com>
2021-11-10 09:39:50 +0100

    Initial commit

---
+++ src/main/java/com/example/httpsexercise/HttpSexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.httpsexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpSexerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpSexerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/httpsexercise/HttpSexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.httpsexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpSexerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
